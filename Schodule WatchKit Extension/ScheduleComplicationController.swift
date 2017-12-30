406788740104e547b51c81623ebdb8ac5d4f3b2c
Nicholas714 <grananicholas@gmail.com>
2017-12-30 12:39:22 -0500

    added icons and added auto updating

--- Schodule WatchKit Extension/ScheduleComplicationController.swift
+++ Schodule WatchKit Extension/ScheduleComplicationController.swift
@@ -10,6 +10,9 @@ import ClockKit
 
 class ScheduleComplicationController: NSObject, CLKComplicationDataSource {
     
+    // MARK: Properties
+    
+    // start of the schedule (set to 7:05 am)
     var morningStart: Date {
         get {
             let calender = Calendar.current
@@ -17,18 +20,43 @@ class ScheduleComplicationController: NSObject, CLKComplicationDataSource {
         }
     }
     
-    // TODO: adding to next day
-    lazy var schedule = {
-        return Schedule(start: morningStart, classPeriodTime: 40, hallTime: 5, periods: "Intro to Crim", "Electronics 1", "Stats", "Lunch", "Calc", "APCS", "English", "GYM", "Physics")
-    }()
     
-    func complicationTemplate(_ complication: CLKComplication, from date: Date = Date()) -> CLKComplicationTemplate? {
+    // actual schedule which will be generated based on classPeriodTime and hallTime
+    var schedule: Schedule {
+        return Schedule(start: morningStart, classPeriodTime: 40, hallTime: 5, periods: "Intro to Criminology", "Electronics 1", "AP Statistics", "Lunch", "AP Calculus", "AP Comp Science", "AP Literature", "Physical Education", "AP Physics")
+    }
+    
+    // MARK - Model functions
+    
+    func getNextRequestedUpdateDate(handler: @escaping (Date?) -> Void) {
+        handler(morningStart.addingTimeInterval(86405))
+    }
+    
+    func requestedUpdateDidBegin() {
+        let server=CLKComplicationServer.sharedInstance()
+        
+        guard let active = server.activeComplications else {
+            return
+        }
+        
+        for complication in active {
+            server.reloadTimeline(for: complication)
+        }
+    }
+    
+    // given a date and compliation type, this will send back the current template
+    func complicationTemplate(_ complication: CLKComplication, from date: Date = Date(), blank: Bool = false) -> CLKComplicationTemplate? {
         switch complication.family {
         case .modularLarge:
-            print("large")
             let template = CLKComplicationTemplateModularLargeStandardBody()
 
-            if let period = schedule.getNextClass(date: date) {
+            if blank {
+                template.headerTextProvider = CLKSimpleTextProvider(text: "Time")
+                template.body1TextProvider = CLKSimpleTextProvider(text: "Class")
+                return template
+            }
+            
+            if let period = schedule.getClass(from: date) {
                 template.headerTextProvider = CLKRelativeDateTextProvider(date: period.finishDate, style: .natural, units: [.minute])
                 template.body1TextProvider = CLKSimpleTextProvider(text: period.name)
             } else {
@@ -39,13 +67,17 @@ class ScheduleComplicationController: NSObject, CLKComplicationDataSource {
             return template
 
         case .modularSmall:
-            print("small")
             let template = CLKComplicationTemplateModularSmallSimpleText()
 
-            if let period = schedule.getNextClass(date: date) {
+            if blank {
+                template.textProvider = CLKSimpleTextProvider(text: "--")
+                return template
+            }
+            
+            if let period = schedule.getClass(from: date) {
                 template.textProvider = CLKRelativeDateTextProvider(date: period.finishDate, style: .natural, units: .minute)
             } else {
-                template.textProvider = CLKSimpleTextProvider(text: "--")
+                template.textProvider = CLKSimpleTextProvider(text: "")
             }
 
             return template
@@ -54,10 +86,14 @@ class ScheduleComplicationController: NSObject, CLKComplicationDataSource {
         }
     }
     
+    // MARK: WatchKit functions
+    
+    // supports backwards and forwards for seeing schedule
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward])
+        handler([.backward, .forward])
     }
 
+    // returns the template for the current date
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void)
     {
         if let template = complicationTemplate(complication) {
@@ -65,43 +101,56 @@ class ScheduleComplicationController: NSObject, CLKComplicationDataSource {
         }
     }
 
+    // returns the sample template with default values when first installing complication
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        if let template = complicationTemplate(complication) {
-           handler(template)
-        }
+        handler(complicationTemplate(complication, from: Date(), blank: true))
     }
     
-
+    // makes timeline for the day
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         var timelineEntires = [CLKComplicationTimelineEntry]()
+
+        // adds start entry to disable timer when it goes before
+        let padOne = schedule.start.addingTimeInterval(-1)
+        timelineEntires.append(CLKComplicationTimelineEntry(date: padOne, complicationTemplate: complicationTemplate(complication, from: padOne)!))
         
         var i = 0
-        for period in schedule.periods {
+        
+        for period in schedule.periods where i < limit {
             let timelineEntry: CLKComplicationTimelineEntry
             
             if let template = complicationTemplate(complication, from: period.finishDate) {
-                if period.isHallway {
-                    timelineEntry = CLKComplicationTimelineEntry(date: period.finishDate.addingTimeInterval(-60 * 5), complicationTemplate: template)
-                } else {
-                    timelineEntry = CLKComplicationTimelineEntry(date: period.finishDate.addingTimeInterval(-60 * 40), complicationTemplate: template)
-                }
+                timelineEntry = CLKComplicationTimelineEntry(date: period.finishDate.addingTimeInterval(-60 * period.time), complicationTemplate: template)
             } else {
                 continue
             }
             
             timelineEntires.append(timelineEntry)
             i += 1
-            if limit == i {
-                break
-            }
+        }
+        
+        // adds final entry to disable timer when it goes past
+        if let padTwo = schedule.periods.last?.finishDate.addingTimeInterval(1) {
+            timelineEntires.append(CLKComplicationTimelineEntry(date: padTwo, complicationTemplate: complicationTemplate(complication, from: padTwo)!))
         }
         
         handler(timelineEntires)
     }
     
+    // callback to after date since they use the same functions for displaying past time in time travel mode
+    func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
+        getTimelineEntries(for: complication, after: date, limit: limit, withHandler: handler)
+    }
+    
+    // block time travel for anything before the start of the schedule
+    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
+        handler(schedule.start.addingTimeInterval(-2))
+    }
+    
+    // block time travel for anything after the last schedule entry
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         if let finishDate = schedule.periods.last?.finishDate {
-            handler(finishDate)
+            handler(finishDate.addingTimeInterval(2)) // adds 1 min to account for last entry that disables timer
         }
     }
     
--- Shared/Schedule.swift
+++ Shared/Schedule.swift
@@ -17,35 +17,37 @@ class Schedule {
     var periods = [Period]()
     
     init(start: Date, classPeriodTime: TimeInterval, hallTime: TimeInterval, periods: String...) {
-        print("start \(start)")
         self.start = start
         self.classPeriodTime = classPeriodTime
         self.hallTime = hallTime
         
         var currentTime: TimeInterval = 0
         let periodEndTime = start
+        
         for perName in periods {
+            var periodTime = classPeriodTime
             
             // extend for 5 min for Pledge / Announcements
             if perName == "Electronics 1" {
                 currentTime += 5
+                periodTime += 5
             }
             
             currentTime += classPeriodTime
-            let period = Period(name: perName, finishDate: periodEndTime.addingTimeInterval(currentTime * 60))
+            let period = Period(name: perName, finishDate: periodEndTime.addingTimeInterval(currentTime * 60), time: periodTime)
             self.periods.append(period)
             
             currentTime += hallTime
-            let hallway = Period(name: "Hallway", finishDate: periodEndTime.addingTimeInterval(currentTime * 60), isHallway: true)
+            let hallway = Period(name: "Hallway", finishDate: periodEndTime.addingTimeInterval(currentTime * 60), time: hallTime)
             self.periods.append(hallway)
         }
         
+        // remove hallway from last entry
+        self.periods.removeLast()
     }
     
-    func getNextClass(date: Date = Date()) -> Period? {
-        print(date)
-        for period in periods where date <= period.finishDate {
-            print("hi")
+    func getClass(from date: Date = Date()) -> Period? {
+        for period in periods where date <= period.finishDate && date >= start {
             return period
         }
         
@@ -56,14 +58,14 @@ class Schedule {
 
 class Period {
     
-    let isHallway: Bool
     let name: String
     let finishDate: Date
+    let time: TimeInterval
     
-    init(name: String, finishDate: Date, isHallway: Bool  = false) {
+    init(name: String, finishDate: Date, time: TimeInterval) {
         self.name = name
         self.finishDate = finishDate
-        self.isHallway = isHallway
+        self.time = time
     }
     
 }
