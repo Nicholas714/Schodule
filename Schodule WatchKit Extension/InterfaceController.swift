805a4b46974dfb232113b8c316b0d17c8d573feb
Nicholas714 <grananicholas@gmail.com>
2018-01-23 16:26:16 -0500

    started initial table view implementation

--- Schodule WatchKit Extension/InterfaceController.swift
+++ Schodule WatchKit Extension/InterfaceController.swift
@@ -12,5 +12,28 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
-
+    var morningStart: Date {
+        get {
+            let calender = Calendar.current
+            return calender.date(bySettingHour: 7, minute: 05, second: 0, of: Date())!
+        }
+    }
+    
+    let names = ["Intro to Criminology", "Electronics 1", "AP Statistics", "Lunch", "AP Calculus", "AP Comp Science", "AP Literature", "Physical Education", "AP Physics"]
+    
+    @IBOutlet var scheduleTable: WKInterfaceTable!
+    
+    override func didAppear() {
+        scheduleTable.setNumberOfRows(names.count, withRowType: "mainRowType")
+        for (index, name) in names.enumerated() {
+                let row = scheduleTable.rowController(at: index) as! MainRowType
+                let label = row.rowDescription
+                label?.setText("\(name)")
+        }
+    }
+    
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        // TODO: open information for period
+    }
+    
 }
