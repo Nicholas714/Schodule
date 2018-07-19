e21459cd8790c4509948425a2086736e64eb62ff
Nicholas714 <grananicholas@gmail.com>
2018-07-19 13:06:32 -0400

    added pods which don't work

---
+++ Pods/Target Support Files/Pods-Schoodule/Pods-Schoodule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Schoodule : NSObject
+@end
+@implementation PodsDummy_Pods_Schoodule
+@end
---
+++ Pods/Target Support Files/Pods-SchooduleTests/Pods-SchooduleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SchooduleTests : NSObject
+@end
+@implementation PodsDummy_Pods_SchooduleTests
+@end
